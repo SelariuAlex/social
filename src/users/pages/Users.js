f36f8360d234245f18a1089b61cb8afa7d6f524c
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-02-10 00:02:13 +0200

    user list

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { UserList } from "../components/UserList";
 
 export const Users = () => {
-  return <div>users</div>;
+  const USERS = [
+    {
+      id: 1,
+      name: "Alex Selariu",
+      image:
+        "https://www.capital.ro/wp-content/uploads/2019/10/timisoara-catedrala.jpg",
+      places: 3
+    }
+  ];
+  return <UserList items={USERS} />;
 };
