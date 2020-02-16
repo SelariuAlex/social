1a2d41a88f9017141541c9cfcd1708677360c49e
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-02-16 10:30:59 +0200

    add modal

--- src/shared/components/UIElements/Modal.js
+++ src/shared/components/UIElements/Modal.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
+import Backdrop from "./Backdrop";
 
 import "./Modal.css";
 
@@ -26,6 +28,21 @@ const ModalOverlay = props => {
   return ReactDOM.createPortal(content, document.getElementById("modal-hook"));
 };
 
-const Modal = props => {};
+const Modal = props => {
+  return (
+    <>
+      {props.show && <Backdrop onClick={props.onCancel} />}
+      <CSSTransition
+        in={props.show}
+        mountOnEnter
+        unmountOnExit
+        timeout={200}
+        classNames="modal"
+      >
+        <ModalOverlay {...props} />
+      </CSSTransition>
+    </>
+  );
+};
 
 export default Modal;
