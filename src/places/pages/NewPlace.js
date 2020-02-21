2782df8870a01dc33faf25c9ef9286d40a8ce4ed
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-02-21 17:32:16 +0200

    manage state in input

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -5,7 +5,7 @@ import "./NewPlace.css";
 export const NewPlace = () => {
   return (
     <div className="place-form">
-      <Input element="input" type="text" label="title" />
+      <Input element="input" type="text" label="title" validators={[]} errorText='Please enter a valid title'/>
     </div>
   );
 };
