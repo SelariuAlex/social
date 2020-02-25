407f9a4ca3abc92b9e39065dded71ec5b367baf2
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-02-25 21:29:23 +0200

    managing form

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useReducer } from "react";
 import { Input } from "../../shared/components/FormElements/Input";
 import {
   VALIDATOR_REQUIRE,
@@ -7,7 +7,45 @@ import {
 } from "../../shared/util/validators";
 import "./NewPlace.css";
 
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        input: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid }
+        },
+        isValid: formIsValid
+      };
+    default:
+      return state;
+  }
+};
+
 export const NewPlace = () => {
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false
+      },
+      description: {
+        value: "",
+        isValid: false
+      }
+    },
+    isValid: false
+  });
+
   const titleInputHandler = useCallback((id, value, isValid) => {}, []);
 
   const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
