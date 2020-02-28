f9f11ff74442ed23c55c27d9cb9a1a6f1df18d24
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-02-28 18:18:37 +0200

    update place route

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { Users } from "./users/pages/Users";
 import { NewPlace } from "./places/pages/NewPlace";
 import { MainNavigation } from "./shared/components/Navigation/MainNavigation";
 import { UserPlaces } from "./places/pages/UserPlaces";
+import { UpdatePlace } from "./places/pages/UpdatePlace";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route exact path="/" component={Users}></Route>
           <Route exact path="/:userId/places" component={UserPlaces}></Route>
           <Route exact path="/places/new" component={NewPlace}></Route>
+          <Route exact path="/places/:placeId" component={UpdatePlace}></Route>
           <Redirect to="/" />
         </Switch>
       </main>
